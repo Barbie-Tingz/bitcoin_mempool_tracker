cff871046403b41c7e6ede140c0d443ec429a331
Barbie-Tingz <jillianhenry0823@gmail.com>
2026-06-25 17:19:51 -0400

    Built out estimate_smart_fee file. Included low, medium, and high parameters onto one endpoint.

---
+++ backend/src/routes/estimate_smart_fee_route.rs
@@ -0,0 +1,43 @@
+use reqwest::{Client};
+use axum::{Router, routing::get};
+use axum::extract::{Json, State, Path};
+use crate::model::estimate_smart_fee; 
+
+const BITCOIN: &str = "http://127.0.0.1:8332";
+
+// set up for out routes and data
+pub fn route() -> Router {
+    let app = Router::new(); // directs traffic 
+    let state = Client::new(); 
+
+    app.route("/estimate_smart_fee/:target", get(estimate_smart_fee))
+    .with_state(state) // method on the router that attaches shared data that handlers can use
+}
+
+pub async fn send_request(
+    client: &reqwest::Client,
+    body: serde_json::Value
+) -> Result <estimate_smart_fee::RPCResponse, reqwest::Error> {
+
+    client
+    .post(BITCOIN)
+    .json(&body) // fetches the json body
+    .send() // sends the request
+    .await?
+    .json::<estimate_smart_fee::RPCResponse>()
+    .await?
+}
+
+pub async fn estimate_smart_fee(State(client):State<Client>, Path(target): Path<u64>
+) -> Result<estimate_smart_fee::RPCResponse, reqwest::Error> {
+
+    let body = serde_json::json!(
+        {
+            "jsonrpc": "2.0",
+            "method": "estimatesmartfee",
+            "params": [target],
+            "id": 1
+        }
+    );
+    send_request(&client, body).await
+}
